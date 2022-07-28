80775ddbf6f91b8c1897114049997a2a9e9064ee
Marina Izaguirre <mai.izaguirre@hotmail.com>
2022-07-28 20:05:37 -0300

    commit 28.7

--- src/main/java/com/ejemplo/SpringBoot/controller/ProyectoJpaController.java
+++ src/main/java/com/ejemplo/SpringBoot/controller/ProyectoJpaController.java
@@ -7,17 +7,21 @@ import com.ejemplo.SpringBoot.service.IProyectoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/persona")
+@CrossOrigin(origins = "*")
 public class ProyectoJpaController {
    
     @Autowired    
